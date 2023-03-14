674b98ffab4bb926bd7bd2ae62f4d26815b82721
moritzlueck <127433924+moritzlueck@users.noreply.github.com>
2023-03-14 13:59:43 +0100

    Structure

--- Programm_dm/Atmega2560/CompileAndFlash.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-cd C:\Users\morit\Documents\Microcontroller\Robocup\Robocup_2023\Programm_dm\Atmega2560
-make
-
-cd C:\Program Files (x86)\Arduino\hardware\tools\avr\bin
-avrdude -D -patmega2560 -cwiring -PCOM4 -b115200 -Uflash:w:"$(ProjectDir)Debug\$(TargetName).hex":i -C"C:\Program Files (x86)\Arduino\hardware\tools\avr\etc\avrdude.conf"
